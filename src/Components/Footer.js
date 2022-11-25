31347fdeec00d74793188318379132337be18d97
skyone <1119858711@qq.com>
2022-11-25 16:56:54 +0000

    Add Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components'
 import thanks from '../Images/thanks.png';
+import twitterSVG from '../Images/twitter.svg';
+import openseaSVG from '../Images/opensea.svg';
 
 const Image = styled.img`
   width: 94px;
   height: 92px;
-  margin: 0 40px 20px;
+//   margin: 0 40px 20px;
+//   margin-right: 10px;
+  // make it close the right
+    margin-left: auto;
 `
 
 const Div = styled.div`
@@ -13,10 +18,18 @@ const Div = styled.div`
     flex-direction: row;
 `
 
+const ImageLogo = styled.img`
+width: 32px;
+height: 30px;
+margin: 0 60px;
+`
+
 export default function Footer() {
     return (
         <Div>
-            <Image src={thanks} alt="Thanks"></Image>
+            <ImageLogo src={twitterSVG} alt="twitter" />
+            <ImageLogo src={openseaSVG} alt="opensea" />
+            <Image src={thanks} alt="Thanks"/>
         </Div>
 
     )
